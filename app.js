4931fd5a54122161c25acf522a49db78f951b0ca
artssoria <arts.soria9@gmail.com>
2024-01-26 23:49:34 -0300

    subiendo el form de registtro

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
 
+// Ruta para register.html
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'register.html'));
+});
+
 
 const port = 3000;
 
